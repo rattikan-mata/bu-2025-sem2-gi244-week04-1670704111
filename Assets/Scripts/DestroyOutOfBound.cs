26c41f5782347f4116cec597e4754371b3c4e2e7
rattikan <rattikan.mata@bumail.net>
2026-02-06 16:40:53 +0700

    wip2

--- Assets/Scripts/DestroyOutOfBound.cs
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -2,9 +2,19 @@ using UnityEngine;
 
 public class DestroyOutOfBound : MonoBehaviour
 {
+    public float upRange;
+    public float downRange;
+
     // Update is called once per frame
     void Update()
     {
-
+        if (transform.position.z > 20)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
